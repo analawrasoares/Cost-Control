31cf2b5b403fcaa33849ecea198edaff043398b6
zagamendes <zagamendes@users.noreply.github.com>
2020-03-27 18:18:10 -0300

    CRUD NO FIREBASE COMPLETO
    CRIEI AS FUNÇÕES QUE SALVAM, LISTAM, ALTERAM E EXCLUEM O USUÁRIO DO SISTEMA

---
+++ Cost-Control/www/js/firebase/firebase-init.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyAX_zCkDeCGy_40DlvaUyXVwEaGiCYwEQo",
+    authDomain: "cost-control-59e67.firebaseapp.com",
+    databaseURL: "https://cost-control-59e67.firebaseio.com",
+    projectId: "cost-control-59e67",
+    storageBucket: "cost-control-59e67.appspot.com",
+    messagingSenderId: "308752238043",
+    appId: "1:308752238043:web:b65d2f9aed37087fbb93fe",
+    measurementId: "G-MWBRSY4X7F"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- Cost-Control/www/js/index.js
+++ Cost-Control/www/js/index.js
@@ -1,46 +1,164 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+$(document).ready(function(){
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    var url = location.search;
+    if(url!=""){
+        let id = url.slice(4);
+        console.log(id);
+        trazDadosUser(id);
     }
-};
 
-app.initialize();
+    //ADM CLICOU EM SALVAR
+    $("#form-user").submit((evento)=>{
+
+        //NÃO DEIXA O FORMULÁRIO SER ENVIADO
+
+        evento.preventDefault();
+        if(url!=""){
+            //OBJETO USUÁRIO PARA SER SALVO NO BANCO
+            user={
+                nome:$("#input-nome").val(),
+                cpf:$("#input-cpf").val(),
+                email:$("#input-email").val(),
+                id:$("#input-id").val()
+
+            };
+
+            salvaUser(user,null);
+
+        }else{
+            //OBJETO USUÁRIO PARA SER SALVO NO BANCO
+            user={
+                nome:$("#input-nome").val(),
+                cpf:$("#input-cpf").val(),
+                email:$("#input-email").val(),
+                id:$("#input-id").val(),
+                senha:Math.floor(Math.random() * 65536)  + 32768
+            };
+            
+            //OBJETO EMAIL COM AS INFORMAÇÕES PARA SEREM MANDADAS VIA EMAIL
+            objEmail={
+                Host: "smtp.gmail.com",
+                Username : "costcontrolproject@gmail.com",
+                Password : "trabalhodojean123",
+                To : user.email,
+                From : "costcontrolproject@gmail.com",
+                Subject : "Senha",
+                Body : "Sua senha é:" + user.senha 
+            };
+
+            salvaUser(user,objEmail);
+
+        }
+
+        
+
+        
+        
+        
+
+    });
+
+    //FUNÇÃO QUE EXCLUI O USUÁRIO DO SISTEMA
+    $("table").on("click","#btn-excluir",function(){
+        excluiuser($(this).val());
+        
+    });
+    //TODA VEZ QUE UM NOVO USUÁRIO É ADICIONADO AO BANCO ESSA FUNÇÃO ADICIONA O MESMO NA TABELA
+    firebase.database().ref("usuarios").on("child_added",novoUser=>{
+        $("tbody").append(criaLinha(novoUser));
+    });
+
+    //TODA VEZ QUE UM USUÁRIO É EXCLUIDO DO BANCO ESSA FUNÇÃO O REMOVE DA TABELA
+    firebase.database().ref("usuarios").on("child_removed",user=>{
+        $("#"+user.key).fadeOut(()=>$("#"+user.key).remove());
+    });
+
+
+});
+
+function criaLinha(user){
+
+    linha = `<tr id=${user.key}>
+                <td>${user.val().nome}</td>
+                <td>${user.val().cpf}</td>
+                <td>${user.val().email}</td>
+                <td><a href="cadastroUsuario.html?id=${user.key}" class='btn btn-primary text-uppercase font-weight-bold'>Editar <span class='fas fa-edit'></span></a></td>
+                <td><button id='btn-excluir' value='${user.key}' class='btn btn-danger text-uppercase font-weight-bold'>Excluir <span class='fas fa-trash'></span</button></td>
+            </tr>`;
+
+    return linha;
+}
+
+function salvaUser(user,objEmail){
+
+    //CASO O ID ESTEJA VÁZIO SIGNIFICA QUE É UM USUÁRIO NOVO
+    if(user.id==""){
+
+        //CONECTA COM O BANCO E CRIA UMA CHAVE PRIMÁRIA COM O MÉTODO PUSH E EM SEGUIDA SALVA USUÁRIO NO BANCO
+        firebase.database().ref("usuarios").push().set(user)
+
+        //CASO SALVE COM SUCESSO APARECE MENSAGEM DE SUCESSO E MANDA EMAIL PARA USUÁRIO 
+        .then(()=>{
+            Notificacao.sucesso("Usuário cadastrado com sucesso!");
+            return Email.send(objEmail);
+        })
+
+        //CASO CONSIGA MANDAR EMAIL PARA O USUÁRIO APARECE MENSAGEM DE SUCESSO
+        .then(()=>Notificacao.sucesso("Email enviado com sucesso"))
+            
+        //CASO TENHA DADO ALGUM ERRO EM ALGUMA OPERCAÇÃO ACIMA O ERRO SERÁ MOSTRADO NO CONSOLE E NA NOTIFICAÇÃO
+        .catch(erro=>{
+            console.log(erro);
+            Notificacao.erro(erro);
+        });
+
+    }else{
+
+        //CONECTA COM O BANCO E ATUALIZA OS DADOS DO CLIENTE
+        firebase.database().ref("usuarios/"+user.id).update(user)
+
+        //CASO SALVE COM SUCESSO APARECE MENSAGEM DE SUCESSO E MANDA EMAIL PARA USUÁRIO 
+        .then(()=>{
+            
+            Notificacao.sucesso("Usuário atualizado com sucesso!");
+            //ESPERA UMA QUANTIDADE DE SEGUNDOS E REDIRECIONA PARA A PÁGINA INICIAL
+            return sleep(1500)
+            
+        })
+        .then(()=>location.replace("cadastroUsuario.html"))
+
+        //CASO TENHA DADO ALGUM ERRO EM ALGUMA OPERCAÇÃO ACIMA O ERRO SERÁ MOSTRADO NO CONSOLE E NA NOTIFICAÇÃO
+        .catch(erro=>{
+            console.log(erro);
+            Notificacao.erro(erro);
+        });
+
+    }
+
+    
+}
+function sleep(ms) {
+    
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
+function excluiuser(id){
+    firebase.database().ref("usuarios/"+id).remove()
+    .then(()=>Notificacao.sucesso("Usuário excluido com sucesso!"))
+    .catch(erro=>{
+        console.log(erro);
+        Notificacao.erro(erro);
+    })
+}
+
+function trazDadosUser(id){
+
+    firebase.database().ref("usuarios/"+id).once("value",user=>{
+        console.log(user.val());
+        $("#input-nome").val(user.val().nome);
+        $("#input-cpf").val(user.val().cpf);
+        $("#input-email").val(user.val().email);
+        $("#input-id").val(user.key);
+    })
+}
